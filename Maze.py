710903112d0e5afd53d62a9d6266795f3598a4a2
maucarrui <maucarrui@ciencias.unam.mx>
2020-08-03 22:08:21 -0500

    Maze: Added miner creation to the constructor.

--- Maze.py
+++ Maze.py
@@ -1,6 +1,12 @@
 # Cell class.
 from Cell import *
 
+# Miner class.
+from Miner import *
+
+# Random functions
+import random
+
 class Maze:
     """
     Class for a maze.
@@ -13,6 +19,9 @@ class Maze:
     x_size = None
     # Number of rows the board will have.
     y_size = None
+
+    # The miner of the maze.
+    miner = None
 
     def __init__(self, X, Y):
         """
@@ -31,3 +40,11 @@ class Maze:
         for x in range(X):
             for y in range(Y):
                 self.board[x][y] = Cell(x, y)
+
+        # Places a miner in a random location.
+        initial_X = random.randint(0, X-1)
+        initial_Y = random.randint(0, Y-1)
+
+        self.miner = Miner(initial_X,
+                           initial_Y,
+                           self.board[initial_X][initial_Y])

70cd7253cb0db5983d00d2b6a421476254f876e3
maucarrui <maucarrui@ciencias.unam.mx>
2020-08-06 13:27:42 -0500

    Maze: There are now to travelers (DFS and BFS) and a method to clone the board.

--- Maze.py
+++ Maze.py
@@ -18,6 +18,10 @@ class Maze:
     # The board that represents the maze.
     board = None
 
+    # The board that will be traversed using BFS and DFS.
+    board_BFS = None
+    board_DFS = None
+
     # Number of columns the board will have.
     x_size = None
     # Number of rows the board will have.
@@ -26,8 +30,12 @@ class Maze:
     # The miner of the maze.
     miner = None
 
-    # The traveler of the maze.
-    traveler = None
+    # The travelers of the maze.
+    traveler_BFS = None
+    traveler_DFS = None
+
+    # If the traveler have started.
+    travelers_started = False
 
     def __init__(self, X, Y):
         """
@@ -51,8 +59,9 @@ class Maze:
         random_y = random.randint(0, Y-1)
         self.board[0][random_y].start = True
 
-        # Spawns the traveler in the beginning of the maze.
-        self.traveler = Traveler(self.board[0][random_y])
+        # Spawns the travelers in the beginning of the maze.
+        self.traveler_BFS = Traveler('BFS', self.board[0][random_y])
+        self.traveler_DFS = Traveler('DFS', self.board[0][random_y])
 
         # Sets a random cell as the ending of the maze.
         random_y = random.randint(0, Y-1)
@@ -66,6 +75,21 @@ class Maze:
                            initial_Y,
                            self.board[initial_X][initial_Y])
 
+    def clone_board(self):
+        """
+        Clones the current board.
+        :return: The cloned board.
+        """
+        X = self.x_size
+        Y = self.y_size
+        clone = [ [None for _ in range(Y)] for _ in range(X)]
+
+        for x in range(X):
+            for y in range(Y):
+                clone[x][y] = self.board[x][y].clone()
+
+        return clone
+
     def build_maze_step_by_step(self):
         """
         Builds the maze step by step.
@@ -73,16 +97,22 @@ class Maze:
         if (not self.miner.is_done()):
             self.miner.explore(self.board)
 
-    def solve_maze_step_by_step(self, method):
+    def solve_maze_step_by_step(self):
         """
         Solves the maze step by step.
         :param method: The method that will be used to search the exit.
-        :return: The current state of the traveler or -1 if the miner is 
-            not done.
         """
         if (self.miner.is_done()):
-            return self.traveler.search(self.board, method)
-        else:
-            return -1
+
+            # If the travelers are starting then
+            # we assign them each a board.
+            if not self.travelers_started:
+                self.travelers_started = True
+                self.board_BFS = self.clone_board()
+                self.board_DFS = self.clone_board()
+
+                
+            self.traveler_BFS.search(self.board_BFS, 'BFS')
+            self.traveler_DFS.search(self.board_DFS, 'DFS')
         
 
